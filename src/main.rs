6ae7737cc0335a8b4abd8a8a0999d70167e225f4
jmcph4 <jmcph4.github@gmail.com>
2022-01-01 19:13:23 +1000

    Extract testing logic out of individual tests
    This commit defines the `Testable` trait which abstracts away shared
    testing boilerplate from the individual test bodies themselves.

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,31 @@
 
 use core::panic::PanicInfo;
 
+mod serial;
 mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello, world!";
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
 
     exit_qemu(QemuExitCode::Success);
@@ -22,21 +38,30 @@ fn test_runner(tests: &[&dyn Fn()]) {
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion... ");
     assert_eq!(1, 1);
-    println!("[ok]");
 }
 
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+// our panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello, world{}", "!");
+    serial_println!("Hello, world{}", "!");
 
     #[cfg(test)]
     test_main();
