0fbb8181c6a90399d0dbdbb9e11bec58df6adaff
jmcph4 <jmcph4.github@gmail.com>
2022-01-01 14:31:38 +1000

    Implement custom test framework
    This commit sets up a custom testing environment for unit testing the
    kernel.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
 static HELLO: &[u8] = b"Hello, world!";
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
 
 /// This function is called on panic.
 #[panic_handler]
@@ -17,6 +35,9 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello, world{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
