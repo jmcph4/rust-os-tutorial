a5e0b4a6730bc71877bba037690cd061455847fe
jmcph4 <jmcph4.github@gmail.com>
2024-09-14 14:10:26 +1000

    Refactor into library and add basic integration tests

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,33 +5,26 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+
 use rust_os_tutorial::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    rust_os_tutorial::test_panic_handler(info)
+    rust_os_tutorial::test_panic_handler(info);
 }
 
 #[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
